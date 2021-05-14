949d7d340d1d37aa14e917bfd7f3a17adc55cfa6
neri78 <dikehara@gmail.com>
2021-05-14 17:43:51 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
 
 function App() {
+  const [name, setName] = useState('');
+  const [greeting, setGreeting] = useState('');
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`/api/greeting?name=${encodeURIComponent(name)}`)
+    .then(response => response.json())
+    .then(state => setGreeting(state.greeting)); 
+  }
+
+  // return function continues...
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +27,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Enter your name: </label>
+            <input
+              id="name"
+              type="text"
+              value={name}
+              onChange={handleChange}
+            />
+            <button type="submit">Submit</button>
+          </form>
+          <p>{greeting}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
